00e3204a2742cf39755d3d2cbad18f3d0f0b33f7
Kyle0817 <gn02025752@gmail.com>
2020-12-06 12:52:32 +0800

    學習thymeleaf使用方式

--- src/main/java/com/udemy/controller/BookController.java
+++ src/main/java/com/udemy/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.udemy.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +17,25 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
+	/**
+	 * 獲取書單列表
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/books")
-	public String list() {
-		return "TestPage";
+	public String list(Model model) {
+		List<Book> books = bookService.findAll();
+		model.addAttribute("books", books);
+		return "books";
 	}
 	
-	@GetMapping("/book/{id}")
+	/**
+	 * 獲取書單詳情
+	 * @param id
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/books/{id}")
 	public String detail(@PathVariable long id, Model model) {
 		Book book = bookService.findOne(id);
 		model.addAttribute("book", book);
